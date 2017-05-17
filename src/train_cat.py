7aab2e707788ea1708617802aa57fb1fa092b242
Fahim Imaduddin Dalvi <dalvifahim@gmail.com>
2017-05-17 18:49:50 +0300

    Now training on approximate proposals using selective search.
    We now search VOC for approximate bounding boxes that have high
    overlap with known bounding boxes for the object. We also use
    proposals with very low overlap as background. Training set now
    has 50% positive objects, 25% negative objects and 25% background.
    Stage is set for bounding box regression.
    
    Acc. is 75% with new data setup.

--- src/train_cat.py
+++ src/train_cat.py
@@ -4,12 +4,12 @@ from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 
 from cifar_processor import load_cifar_data
-from voc_processor import load_voc_data
+from voc_processor import *
 
 import numpy as np
 
 batch_size = 32
-epochs = 30
+epochs = 10
 num_classes = 2
 # x_train, y_train, x_val, y_val, x_test, y_test = load_cifar_data(single_class=3, balance_classes=True)
 x_train, y_train, x_val, y_val, x_test, y_test = load_voc_data()
@@ -17,10 +17,10 @@ x_train, y_train, x_val, y_val, x_test, y_test = load_voc_data()
 # Create model
 model = Sequential()
 
-model.add(Conv2D(64, (3, 3), padding='same',
+model.add(Conv2D(32, (3, 3), padding='same',
                  input_shape=x_train.shape[1:]))
 model.add(Activation('relu'))
-model.add(Conv2D(64, (3, 3)))
+model.add(Conv2D(32, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
@@ -33,6 +33,9 @@ model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 
 model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
 model.add(Dense(512))
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
@@ -54,13 +57,29 @@ model.fit(x_train, y_train,
               shuffle=True)
 
 # evaluate the model
-scores = model.evaluate(x_test, y_test, verbose=0)
-print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
- 
-# serialize model to JSON
-model_json = model.to_json()
-with open("model.json", "w") as json_file:
-    json_file.write(model_json)
-# serialize weights to HDF5
-model.save_weights("model.h5")
+y_pred = model.predict(x_test, verbose=0)
+acc = 100.0 * np.sum(np.argmax(y_pred, axis=1) == np.argmax(y_test, axis=1))/y_test.shape[0]
+
+positive_examples = y_test[:, 1].astype(np.int)
+positive_acc = 100.0 * np.sum(np.argmax(y_pred[positive_examples,:], axis=1) == np.argmax(y_test[positive_examples,:], axis=1))/y_test[positive_examples,:].shape[0]
+print("%s: %.2f%%" % ("Accuracy", acc))
+print("%s: %.2f%%" % ("Cat Accuracy", positive_acc)) 
+
+
+processed_file = "processed_images.npz"
+all_samples = load_processed_samples(processed_file)
+x_positive = all_samples[get_class_idx('cat')]
+
+y_pred = model.predict(x_positive, batch_size=64, verbose=True)
+# print y_pred
+correct = 0
+for i in xrange(y_pred.shape[0]):
+    # print y_pred[i]
+    if y_pred[i,1] > 0.5:
+        correct += 1
+print("%s: %0.2f"%("Cat Accuracy", 100.0*correct/y_pred.shape[0]))
+
+
+# Save model
+model.save("model.h5")
 print("Saved model to disk")
--- src/voc_processor.py
+++ src/voc_processor.py
@@ -3,8 +3,10 @@
 #
 # Author: Fahim Dalvi
 
+import dlib
 import numpy as np
 import os
+import random
 import xml.etree.ElementTree
 
 from PIL import Image
@@ -55,7 +57,7 @@ def extract_image(image_name, bounding_box):
 	cropped_im = cropped_im.resize((64,64), resample=Image.BILINEAR)
 	return np.array(cropped_im)
 
-def process_voc_data(save_file):
+def process_voc_classification_data(save_file):
 	image_sets = [get_class_image_names(c) for c in classes]
 	bounding_boxes = []
 	for c in classes:
@@ -83,6 +85,117 @@ def process_voc_data(save_file):
 
 	np.savez_compressed("processed_images", **all_samples)
 
+def IoU(bbox1, bbox2):
+	inter_x1 = max(bbox1[0], bbox2[0])
+	inter_y1 = max(bbox1[1], bbox2[1])
+	inter_x2 = min(bbox1[2], bbox2[2])
+	inter_y2 = min(bbox1[3], bbox2[3])
+
+
+	if inter_y1 > inter_y2 or inter_x1 > inter_x2:
+		return 0.0
+
+	interection_area = float((inter_x2 - inter_x1) * (inter_y2 - inter_y1))
+	bbox1_area = (bbox1[2] - bbox1[0]) * (bbox1[3] - bbox1[1])
+	bbox2_area = (bbox2[2] - bbox2[0]) * (bbox2[3] - bbox2[1])
+
+	# Check if boxes are exactly equal
+	if (bbox1_area + bbox2_area - interection_area) == 0:
+		return 1
+	return interection_area / (bbox1_area + bbox2_area - interection_area)
+
+def process_voc_detection_data(save_file):
+	image_sets = [get_class_image_names(c) for c in classes]
+	bounding_boxes = []
+	for c in classes:
+		print "Extracting boxes for",c
+		bounding_boxes.append([get_bounding_boxes(image_sets[get_class_idx(c)][i], c) for i in xrange(len(image_sets[get_class_idx(c)]))])
+
+	all_samples = {}
+	background_samples = []
+	background_bbox_deltas = []
+	for c in classes:
+		print "Extracting images for",c
+		class_idx = get_class_idx(c)
+		num_images = len(image_sets[class_idx])
+		num_samples = sum([len(bboxes) for bboxes in bounding_boxes[class_idx]])
+		samples = np.zeros((num_samples, 64, 64, 3), dtype=np.int8)
+		samples = []
+		bbox_deltas = []
+		ious = []
+		sample_idx = 0
+
+		progress_meter = int(0.10 * num_images)
+
+		for image_idx, image_name in enumerate(image_sets[class_idx]):
+			if (image_idx+1) % progress_meter == 0:
+				print int(100.0 * (image_idx+1)/num_images),"% done"
+			for bbox in bounding_boxes[class_idx][image_idx]:
+				im = Image.open(DATA_PATH + 'JPEGImages/' + image_name + ".jpg")
+				img = np.array(im)
+				regions = []
+				dlib.find_candidate_object_locations(img, regions, min_size=500)
+
+				positive_bounding_boxes = []
+				negative_bounding_boxes = []
+				for r in regions:
+					if len(positive_bounding_boxes) > 10 and len(negative_bounding_boxes) > 10:
+						break
+					curr_bbox = [r.left(), r.top(), r.right(), r.bottom()]
+					iou = IoU(bbox, curr_bbox)
+					if iou > 0.5:
+						positive_bounding_boxes.append(curr_bbox + [iou])
+
+					if iou < 0.3:
+						negative_bounding_boxes.append(curr_bbox + [iou])
+
+				random.shuffle(positive_bounding_boxes)
+				random.shuffle(negative_bounding_boxes)
+
+				positive_bounding_boxes = positive_bounding_boxes[:4]
+				negative_bounding_boxes = negative_bounding_boxes[:3]
+
+				# Get ground_truth
+				cropped_im = im.crop(bbox)
+				cropped_im = cropped_im.resize((64,64), resample=Image.BILINEAR)
+				samples.append(np.array(cropped_im))
+				bbox_deltas.append([0,0,0,0])
+				ious.append(IoU(bbox, bbox))
+
+				# Get positive samples
+				for p_bbox in positive_bounding_boxes:
+					cropped_im = im.crop(p_bbox[:4])
+					cropped_im = cropped_im.resize((64,64), resample=Image.BILINEAR)
+					samples.append(np.array(cropped_im))
+					bbox_deltas.append([
+						bbox[0]-p_bbox[0],
+						bbox[1]-p_bbox[1],
+						bbox[2]-p_bbox[2],
+						bbox[3]-p_bbox[3],
+					])
+					ious.append(p_bbox[4])
+
+				# Get negative samples
+				for n_bbox in negative_bounding_boxes:
+					cropped_im = im.crop(n_bbox[:4])
+					cropped_im = cropped_im.resize((64,64), resample=Image.BILINEAR)
+					background_samples.append(np.array(cropped_im))
+					background_bbox_deltas.append([0,0,0,0])
+		samples = np.stack(samples, axis=0)
+		bbox_deltas = np.stack(bbox_deltas, axis=0)
+		print c,samples.shape, bbox_deltas.shape,num_samples
+		all_samples["samples_" + c] = samples
+		all_samples["bboxdeltas_" + c] = bbox_deltas
+		# all_samples["ious_" + c] = ious
+
+	background_samples = np.stack(background_samples, axis=0)
+	background_bbox_deltas = np.stack(background_bbox_deltas, axis=0)
+	print "background",background_samples.shape, background_bbox_deltas.shape
+	all_samples["samples_background"] = background_samples
+	all_samples["bboxdeltas_background"] = background_bbox_deltas
+
+	np.savez_compressed(save_file, **all_samples)
+
 def load_processed_samples(save_file):
 	samples = np.load(save_file)
 	all_samples = []
@@ -90,11 +203,131 @@ def load_processed_samples(save_file):
 		all_samples.append(samples[c])
 	return all_samples
 
+def visualize_processed_files(save_file, num_samples=10):
+	samples = np.load(save_file)
+	print("Loaded data.")
+	# all_samples = []
+	# for c in classes + ["background"]:
+	# 	all_samples.append(samples["samples_" + c])
+	# 	all_samples.append(samples["bboxdeltas_" + c])
+	num_classes = 20
+	full_image = np.zeros((64*num_classes, 64*num_samples, 3), dtype=np.int8)
+	print len(classes + ["background"])
+	for c_idx, c in enumerate(classes + ["background"]):
+		# print samples["samples_" + c].shape
+		# print samples["bboxdeltas_" + c].shape
+		# print samples["ious_" + c].shape
+		idx = np.random.permutation(samples["samples_" + c].shape[0])
+		idx = idx[:num_samples]
+		random_samples = samples["samples_" + c][idx]
+		random_bboxes = samples["bboxdeltas_" + c][idx]
+		# random_ious = samples["ious_" + c][idx]
+
+		# print c_idx
+		for s_idx in xrange(num_samples):
+			tmp = random_samples[s_idx,:,:,:]
+			# print tmp.shape, s_idx, full_image[s_idx*64:(s_idx+1)*64,c_idx*64:(c_idx+1)*64,:].shape
+			# print full_image.shape
+			full_image[c_idx*64:(c_idx+1)*64,s_idx*64:(s_idx+1)*64,:] = tmp
+			# print random_bboxes[s_idx], random_ious[s_idx]
+		# break
+	img = Image.fromarray(full_image , mode="RGB")
+	img.save("full_samples.png")
+
 def load_voc_data():
-	NEGATIVE_SAMPLES = 3
+	NEGATIVE_SAMPLES = 1
+	processed_file = "processed_images_detection.npz"
+	if not os.path.isfile(processed_file):
+		process_voc_detection_data(processed_file)
+
+	all_samples = np.load(processed_file)
+	print("Loaded data.")
+
+	negative_classes = [c for c in classes if c != 'cat']
+	background_class = "background"
+
+	# Collect positive samples
+	x_positive = all_samples["samples_cat"]
+	num_positive = x_positive.shape[0]
+	idx = np.random.permutation(num_positive)
+	train_start, train_end = 0, int(0.7*num_positive)
+	val_start, val_end = train_end+1, int(0.9*num_positive)
+	test_start, test_end = val_end, num_positive
+	x_train_positive = x_positive[idx[train_start:train_end]]
+	x_val_positive = x_positive[idx[val_start:val_end]]
+	x_test_positive = x_positive[idx[test_start:test_end]]
+
+	# Collect negative samples
+	x_negatives = []
+	for c in negative_classes:
+		x_negatives.append(all_samples["samples_" + c])
+	x_negative = np.concatenate(tuple(x_negatives), 0)
+	idx = np.random.permutation(x_negative.shape[0])
+	num_negative_objs = int(NEGATIVE_SAMPLES*0.5*x_positive.shape[0])
+	x_negative = x_negative[idx[:num_negative_objs]]
+
+	x_background = all_samples["samples_background"]
+	idx = np.random.permutation(x_background.shape[0])
+	num_negative_background = num_negative_objs
+	x_background = x_background[idx[:num_negative_background]]
+
+	x_negative = np.concatenate(tuple([x_negative, x_background]), 0)
+	idx = np.random.permutation(x_negative.shape[0])
+	x_negative = x_negative[idx]
+
+	num_negative = num_negative_objs + num_negative_background
+	train_start, train_end = 0, int(0.7*num_negative)
+	val_start, val_end = train_end+1, int(0.9*num_negative)
+	test_start, test_end = val_end, num_negative
+	x_train_negative = x_negative[train_start:train_end]
+	x_val_negative = x_negative[val_start:val_end]
+	x_test_negative = x_negative[test_start:test_end]
+
+	print x_train_negative.shape
+	print x_val_negative.shape
+	print x_test_negative.shape
+
+	print "Num cats:",num_positive
+	print "Num other objects:",num_negative_objs
+	print "Num random background:",num_negative_background
+
+	x_train = np.concatenate((x_train_positive, x_train_negative),0)
+	y_train = np.concatenate((np.ones(x_train_positive.shape[0], dtype=np.int), np.zeros(x_train_negative.shape[0], dtype=np.int)), 0)
+	idx = np.random.permutation(x_train.shape[0])
+	x_train = x_train[idx]
+	y_train = transform_to_2d(y_train[idx])
+
+	x_val = np.concatenate((x_val_positive, x_val_negative),0)
+	y_val = np.concatenate((np.ones(x_val_positive.shape[0], dtype=np.int), np.zeros(x_val_negative.shape[0], dtype=np.int)), 0)
+	idx = np.random.permutation(x_val.shape[0])
+	x_val = x_val[idx]
+	y_val = transform_to_2d(y_val[idx])
+
+	x_test = np.concatenate((x_test_positive, x_test_negative),0)
+	y_test = np.concatenate((np.ones(x_test_positive.shape[0], dtype=np.int), np.zeros(x_test_negative.shape[0], dtype=np.int)), 0)
+	idx = np.random.permutation(x_test.shape[0])
+	x_test = x_test[idx]
+	y_test = y_test[idx]
+	y_test = transform_to_2d(y_test)
+
+	x_train = x_train.astype('float32')
+	x_val = x_val.astype('float32')
+	x_test = x_test.astype('float32')
+	x_train /= 255
+	x_val /= 255
+	x_test /= 255
+
+	print "Num Train:",x_train.shape[0]
+	print "Num Val:",x_val.shape[0]
+	print "Num Test:",x_test.shape[0]
+
+	return x_train, y_train, x_val, y_val, x_test, y_test
+
+def load_voc_classification_data():
+	NEGATIVE_SAMPLES = 1
 	processed_file = "processed_images.npz"
 	if not os.path.isfile(processed_file):
-		process_voc_data(processed_file)
+		process_voc_classification_data(processed_file)
 	all_samples = load_processed_samples(processed_file)
 
 	negative_classes = [c for c in classes if c != 'cat']
@@ -148,6 +381,13 @@ def load_voc_data():
 	y_test = y_test[idx]
 	y_test = transform_to_2d(y_test)
 
+	x_train = x_train.astype('float32')
+	x_val = x_val.astype('float32')
+	x_test = x_test.astype('float32')
+	x_train /= 255
+	x_val /= 255
+	x_test /= 255
+
 	print "Num Train:",x_train.shape[0]
 	print "Num Val:",x_val.shape[0]
 	print "Num Test:",x_test.shape[0]
@@ -155,9 +395,11 @@ def load_voc_data():
 	return x_train, y_train, x_val, y_val, x_test, y_test
 
 def main():
-	load_voc_data()	
-
-
+	# x_train, y_train, x_val, y_val, x_test, y_test = load_voc_data()
+	# print np.sum(y_test, axis=0)
+	# process_voc_detection_data('processed_images_detection')
+	# visualize_processed_files('processed_images_detection.npz')
+	load_voc_data()
 
 if __name__ == '__main__':
 	main()
