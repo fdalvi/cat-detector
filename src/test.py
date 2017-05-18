761ae9c47b87e727aa3f6b847f48cf9271df7714
Fahim Imaduddin Dalvi <dalvifahim@gmail.com>
2017-05-18 11:38:45 +0300

    Better image format handling.

--- src/test.py
+++ src/test.py
@@ -3,7 +3,6 @@ import numpy as np
 
 from PIL import Image
 from PIL import ImageDraw
-from keras.models import model_from_json
 from keras.models import load_model
 
 from voc_processor import *
@@ -13,6 +12,7 @@ import os
 import cPickle as cp
 
 import sys
+import h5py
 
 # Malisiewicz et al.
 def non_max_suppression_fast(boxes, overlapThresh):
@@ -71,77 +71,51 @@ def non_max_suppression_fast(boxes, overlapThresh):
 	# integer data type
 	return boxes[pick].astype("int")
 
-model = load_model('model.h5')
+def normalize_image(img):
+	if len(img.shape) == 2:
+		# Black and White Image
+		img = np.reshape(img, (img.shape[0],img.shape[1],1))
+		img = np.repeat(img, 3, axis=2)
+	elif img.shape[2] == 4:
+		# With Alpha
+		img = img[:,:,0:3]
+	return img
+
+f = h5py.File('model_final.h5', 'r+')
+if 'optimize_weights' in f:
+	del f['optimizer_weights']
+f.close()
+
+model = load_model('model_final.h5')
 
 TEST_SET_PATH = "../../HiringExercise_MLCVEngineer/test_set/"
 # TEST_SET_PATH = "../data/voc2012/VOC2012/JPEGImages/"
 
-# processed_file = "processed_images.npz"
-# all_samples = load_processed_samples(processed_file)
-# x_positive = all_samples[get_class_idx('cat')]
-
-# for i in xrange(x_positive.shape[0]):
-#     iii = Image.fromarray(x_positive[i], mode='RGB')
-#     iii.save("test/results/" + str(i) + ".png")
-
-# # y_pred = model.predict(x_positive, batch_size=64, verbose=True)
-# # # print y_pred
-# # for i in xrange(y_pred.shape[0]):
-# #     print y_pred[i]
-# #     if y_pred[i,1] > 0.5:
-# #         print 'detected!!!'
-
-# import sys
-# sys.exit(1)
 all_regions = dict()
+
 for idx, f in enumerate(os.listdir(TEST_SET_PATH)):
 	print "Image: " + f
 	im = Image.open(TEST_SET_PATH + f)
-	# im = Image.open("../data/voc2012/VOC2012/JPEGImages/2007_003778.jpg")
 	img = np.array(im)
-	if len(img.shape) == 2:
-		# Black and White Image
-		img = np.tile(img, 2)
-	elif img.shape[2] == 4:
-		# With Alpha
-		img = img[:,:,0:3]
+	img = normalize_image(img)
 
-	# img_lbl, regions = selectivesearch.selective_search(img, scale=500, sigma=0.9, min_size=10)
 	all_regions[f] = []
 	dlib.find_candidate_object_locations(img, all_regions[f], min_size=500)
 	print len(all_regions[f])
 	candidates = list()
 	for i, r in enumerate(all_regions[f]):
-	#     # excluding same rectangle (with different segments)
-	#     # if r['rect'] in candidates:
-	#     #     continue
-	#     # # excluding regions smaller than 2000 pixels
-	#     # if r['size'] < 2000:
-	#     #     continue
-	#     # # distorted rects
-	#     # x, y, w, h = r['rect']
-	#     # if w / h > 1.2 or h / w > 1.2:
-	#     #     continue
 		candidates.append([r.left(), r.top(), r.right(), r.bottom()])
 		if i > 10000:
+			# Hard limit number of proposals
 			break
 
-	# # print len(regions)
 	candidates = list(candidates)
-	print "Num candidates:",len(candidates)
-
-	X_test = np.zeros((len(candidates), 64, 64, 3))
-
+	X_test = np.zeros((len(candidates), 128, 128, 3))
 	for idx, bbox in enumerate(candidates):
-		# x,y,w,h = bbox
-		# bounding_box = [x, y, x+w, y+h]
 		bounding_box = bbox
-		# print im.size, bbox, bounding_box
 		cropped_im = im.crop(bounding_box)
-		cropped_im = cropped_im.resize((64,64), resample=Image.BILINEAR)
-		# cropped_im.save("test/results/" + str(idx) + ".png")
-		# im.save("test/00" + str(idx) + ".png")
-		X_test[idx,:,:,:] = np.array(cropped_im)[:,:,0:3]
+		cropped_im = cropped_im.resize((128,128), resample=Image.BILINEAR)
+		X_test[idx,:,:,:] = normalize_image(np.array(cropped_im))
 
 	X_test = X_test.astype('float32')
 	X_test /= 255
@@ -164,17 +138,4 @@ for idx, f in enumerate(os.listdir(TEST_SET_PATH)):
 	for i in xrange(final_bboxes.shape[0]):
 		draw.rectangle(list(final_bboxes[i]), outline="green")
 	del draw
-	im.save("test/results/" + f + ".png", "PNG")
-	break
-	# for idx, bbox in enumerate(candidates):
-	#     x,y,w,h = bbox
-	#     bounding_box = [x, y, x+w, y+h]
-	#     if y_pred[idx,1] > 0:
-	#         draw.rectangle(bounding_box, outline="green")
-	#     else:
-	#         draw.rectangle(bounding_box, outline="red")
-	# del draw
-	# im.save("a.png", "PNG")
-
-# with open("regions.pkl",'w') as fp:
-#     cp.dump(all_regions, fp)
+	im.save("test/results/" + f + ".png", "PNG")	
